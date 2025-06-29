dc29ed5c86c2869a9d6bc3ff5cc97997cfee0b1d
HarishK <harishk.cs1@gmail.com>
2025-06-29 20:54:13 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base :"fixmydb-ui-reimagined",
+  base :"/",
   server: {
     host: "::",
     port: 8080,
